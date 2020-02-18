e12ac400d8909dbc3edaff1a278f8145dcdb7437
Jake Garcia <bballwiz4@gmail.com>
2020-02-18 17:16:27 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,11 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == ""
+  if board[index] == " "
     return false
+  elsif board[index] == ""
+    return false
+  elsif board[index] == nil
+    return false
+    
   end
 end
